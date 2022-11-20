07534ac50280744a521021ff65c2741007b7f7a2
Adam L Barrett <adamlbarrett@hotmail.com>
2022-11-19 22:21:58 -0600

    fix: 🐛 fix select dropdown arrow

--- src/lib/components/RoleSelect.svelte
+++ src/lib/components/RoleSelect.svelte
@@ -46,4 +46,19 @@
     border-radius: 0.5em;
     font-size: 1rem;
   }
+
+  select {
+    -webkit-appearance: none;
+    -moz-appearance: none;
+    background: transparent;
+    background-image: url("data:image/svg+xml;utf8,<svg fill='black' height='24' viewBox='0 0 24 24' width='24' xmlns='http://www.w3.org/2000/svg'><path d='M7 10l5 5 5-5z'/><path d='M0 0h24v24H0z' fill='none'/></svg>");
+    background-repeat: no-repeat;
+    background-position-x: calc(100% - 5px);
+    background-position-y: 5px;
+    border: 1px solid #dfdfdf;
+    border-radius: 2px;
+    margin-right: 2rem;
+    padding: 1rem;
+    padding-right: 2rem;
+  }
 </style>
